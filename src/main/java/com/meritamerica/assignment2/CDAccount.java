e35dfed56825218afd5ed1430aa53e4ae5ebb05f
Gio <giovanni.lmartin@gmail.com>
2020-10-07 23:34:16 -0500

    Revert "commit 10:56"
    This reverts commit a27027998bf712dc0f54a8c9fff7a87f4a41f521.

--- src/main/java/com/meritamerica/assignment2/CDAccount.java
+++ src/main/java/com/meritamerica/assignment2/CDAccount.java
@@ -5,9 +5,11 @@ import java.util.Date;
 public class CDAccount extends BankAccount {
 
 	private double balance;
+	private double interestRate;
 	private int term;
 	private Date date;
 	private double futureValue;
+<<<<<<< HEAD
 	private CDOffering offering;
 <<<<<<< HEAD
 	
@@ -23,14 +25,18 @@ public class CDAccount extends BankAccount {
 	this.offering = offering;
 	this.balance = balance;
 >>>>>>> a27027998bf712dc0f54a8c9fff7a87f4a41f521
+=======
+	
+	CDAccount(CDOffering offering, double balance){
+		super(balance,0);
+>>>>>>> parent of a270279... commit 10:56
 	}
 	
 	public double getBalance() {
-		
 		return this.balance;
-		
 	}
 	public double getInterestRate() {
+<<<<<<< HEAD
 <<<<<<< HEAD
 		return this.offering.getInterestRate();
 	}
@@ -44,15 +50,21 @@ public class CDAccount extends BankAccount {
 		
 		return offering.getTerm();
 >>>>>>> a27027998bf712dc0f54a8c9fff7a87f4a41f521
+=======
+		return this.interestRate;
+	}
+	public int getTerm() {
+		return this.term;
+>>>>>>> parent of a270279... commit 10:56
 	}
 	
 	public Date getStartDate() {
 		return this.date;
 	}
 	public long getAccountNumber() {
-		return super.getAccountNumber();
+		return this.accountNumber;
 	}
 	public double futureValue() {
-		return super.futureValue(getTerm());
+		return this.futureValue;
 	}
 }
